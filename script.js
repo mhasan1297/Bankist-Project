11b2dc3280b20f408851e04a840526ca870500c5
M Hasan <moynul.hasan@hotmail.com>
2024-01-16 20:01:03 +0000

    Added usernames

--- script.js
+++ script.js
@@ -61,6 +61,37 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+    containerMovements.innerHTML = ''
+
+    movements.forEach(function(mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+      const html = 
+        `<div class="movements__row">
+            <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+            <div class="movements__value">${mov}</div>
+        </div>`
+
+        containerMovements.insertAdjacentHTML('afterbegin', html)
+
+    })
+}
+
+displayMovements(account1.movements)
+
+const createUsername = function (accs) {
+    accs.map((acc) =>
+      acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('')
+  );
+};
+
+createUsername(accounts)
+console.log(accounts)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
